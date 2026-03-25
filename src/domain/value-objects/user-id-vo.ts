8dc81264134535bc18f24b236aa21342e14583d7
Poian3k <bassokevin03@gmail.com>
2026-03-25 16:07:59 +0100

    Added userId value object, condensed tests in 1 command and added the trasformation from ESM to CJS for uuid

---
+++ src/domain/value-objects/user-id-vo.ts
@@ -0,0 +1,29 @@
+import { validate as uuidValidate } from 'uuid';
+
+export class UserId {
+  private readonly _value: string;
+
+  private constructor(value: string) {
+    this.validate(value);
+    this._value = value;
+  }
+
+  public static create(value: string): UserId {
+    return new UserId(value);
+  }
+
+  private validate(value: string): void {
+    if (!uuidValidate(value)) {
+      throw new Error('Invalid UUID format for UserId : ' + value);
+    }
+  }
+
+  public equals(other: UserId): boolean {
+    if (!(other instanceof UserId)) return false;
+    return this._value === other._value;
+  }
+
+  public get value(): string {
+    return this._value;
+  }
+}
