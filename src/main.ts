721ebae9c5f0227d29d2344f775b29f4d8dacc6d
Suar <albertosuar2004@gmail.com>
2026-04-18 21:17:37 +0200

    Minor fixes of main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = app.get(ConfigurationService);
   const port = config.port;
   await app.listen(port);
